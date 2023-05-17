147d4d505476a9e0e294396228cf181d0f2d1de4
root <root@BCSTFLAP351.BC.21C.CAMPUS>
2023-05-17 21:09:04 +0100

    Adds net-zero-module-2 to repository.

--- course-ul.sh
+++ course-ul.sh
@@ -26,8 +26,7 @@ for f in *.zip; do
 
     unzip -q $f content/* -d $folder_name; mv $folder_name/content/* $folder_name/; rm -r $folder_name/content
 
-    no_index_follow="<meta name='robots' content='noindex,nofollow'>"
-    sed '/<head>/a{$no_index_follow}'  $folder_name/index.html >  $folder_name/temp.html && mv  $folder_name/temp.html  $folder_name/index.html
+    sed '/<head>/a$<meta name='robots' content='noindex,nofollow'>'  $folder_name/index.html >  $folder_name/temp.html && mv  $folder_name/temp.html  $folder_name/index.html
     
     echo "Finished unzipping ${f}. Deploying to Github now." 
     git add --all; git commit -m "Adds $folder_name to repository."; git push origin main
