ea2a56b12f0f8ca3ce8aa4c4e7eec1b041c3d75a
BC-mjdavies <107399825+BC-mjdavies@users.noreply.github.com>
2023-09-30 20:38:55 +0100

    Reupload again

---
+++ british-values/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://VRb9goHuDHMpiznm6pSPwQvrTSOK46T1_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "British Values | Gwerthoedd Prydeinig"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;Welcome to this tutorial session on British Values and Radicalisation. In this tutorial you will explore the meaning of British values and the importance of this in determining our democratic society. You will also explore the meaning of radicalisation and the threat that this poses to our democratic state.&amp;nbsp;&lt;/p&gt;"
+};
