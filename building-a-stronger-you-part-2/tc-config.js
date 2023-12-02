0b3993a6ed0e653373e3e06b2b3905f9735db29d
BC-mjdavies <107399825+BC-mjdavies@users.noreply.github.com>
2023-12-02 17:40:56 +0000

    Add files via upload

---
+++ building-a-stronger-you-part-2/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://o_WRCdnkNNjJFjh2SIXsMaI9fJoncvCo_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Building stronger you level 2"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": ""
+};
