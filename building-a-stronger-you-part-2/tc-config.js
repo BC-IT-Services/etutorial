242920ab1bc15699b5c4d3b2a225411e46857b31
BC-mjdavies <107399825+BC-mjdavies@users.noreply.github.com>
2023-12-03 01:25:21 +0000

    Add files via upload

---
+++ building-a-stronger-you-part-2/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://WQyzeOfMDu6ikiJPBvzL0rLpEscLF38i_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Building A Stronger You part 2/ Adeiladu Chi Cryfach rhan 2"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;Sesiwn diwtorial yr wythnos hon byddwch yn archwilio&amp;rsquo;r cymorth sydd ar gael yng Ngholeg Penybont. Byddwch yn dysgu mwy am Les, y t&amp;icirc;m a sut y gallant eich cefnogi yn fwy manwl.&amp;nbsp;&lt;/p&gt;&lt;p&gt;This week tutorial session you will explore support available at Bridgend College.&lt;/p&gt;&lt;p&gt;You will learning more about Wellbeing, the team and how they can support you in more detail.&lt;/p&gt;"
+};
