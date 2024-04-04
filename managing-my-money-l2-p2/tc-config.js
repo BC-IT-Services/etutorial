eb316f415447dad329b8854ad891ab0a742ff496
BC-mjdavies <107399825+BC-mjdavies@users.noreply.github.com>
2024-04-04 02:12:18 +0100

    Adding managing my money level 2 part 2

---
+++ managing-my-money-l2-p2/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://QxDhcKWK8fyAI-vHWp6SZApRv9ipck4u_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Managing My Money level-2 (Part -2)"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": ""
+};
