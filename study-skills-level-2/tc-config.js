e76959a1cc969c37217dbeef9eb8fd50477feccf
BC-mjdavies <107399825+BC-mjdavies@users.noreply.github.com>
2024-04-16 13:22:04 +0100

    Add files via upload

---
+++ study-skills-level-2/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://UVsKtH0wrntbdCzRHNoqLhCko-PUgxRq_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Study Skills - Level -2"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;How to Study Effectively? &lt;/p&gt;"
+};
