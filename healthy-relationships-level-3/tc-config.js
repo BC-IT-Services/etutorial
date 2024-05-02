1a020798140e1f87d0be8aafb6f9ea0c22ae349c
BC-mjdavies <107399825+BC-mjdavies@users.noreply.github.com>
2024-05-02 23:16:42 +0100

    Add files via upload

---
+++ healthy-relationships-level-3/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://wJpn1wEPAfrTJVgznrI1dO0QEIXJ1w0V_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Healthy relationships (Level-3)"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;In this e-tutorial, you will:&lt;/p&gt;&lt;ol&gt;&lt;li&gt;Learn about the characteristics of healthy and unhealthy relationships.&lt;/li&gt;&lt;li&gt;Explore how to seek help or support with relationships when you need it.&lt;/li&gt;&lt;/ol&gt;"
+};
