5f16421f4e453a79efb6353d144c0d76ff954897
BC-mjdavies <107399825+BC-mjdavies@users.noreply.github.com>
2024-05-16 16:23:33 +0100

    Add files via upload

---
+++ me-and-my-wellbeing-level-2/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://0jn9g2_qx_09Zzw-tWBGjIR6r0_jz2Du_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Me &amp; My Wellbeing (Level-2)"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;Good choices lead to good wellbeing.&amp;nbsp;&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;This e-tutorial you will be able to understand:&lt;/p&gt;&lt;ul&gt;&lt;li&gt;What is good wellbeing.&lt;/li&gt;&lt;li&gt;How to avoid poor habits.&lt;/li&gt;&lt;li&gt;How to manage stress.&lt;/li&gt;&lt;/ul&gt;"
+};
